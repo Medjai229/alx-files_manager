eea930edb468786d59fd861df70f6f2531644518
Malik <malikhussein229@gmail.com>
2024-09-28 14:22:59 +0300

    Task#0 Redis utils

---
+++ utils/redis.js
@@ -0,0 +1,40 @@
+import { createClient } from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+    this.isConnected = true;
+
+    this.client.on('error', (err) => {
+      console.error(`Redis client not connected to the server: ${err}`);
+      this.isConnected = false;
+    });
+
+    this.client.on('connect', () => {
+      this.isConnected = true;
+    });
+  }
+
+  isAlive() {
+    return this.isConnected;
+  }
+
+  async get(key) {
+    const getAsync = promisify(this.client.get).bind(this.client);
+    return getAsync(key);
+  }
+
+  async set(key, value, duration) {
+    const setAsync = promisify(this.client.setex).bind(this.client);
+    await setAsync(key, duration, value);
+  }
+
+  async del(key) {
+    const delAsync = promisify(this.client.del).bind(this.client);
+    await delAsync(key);
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
