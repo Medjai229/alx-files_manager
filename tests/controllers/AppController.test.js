191a5e640a26fe792fdd7e3bd1c5e0ea8076693b
Malik <malikhussein229@gmail.com>
2024-10-01 21:51:14 +0300

    Task#10 Tasks: Reformating AppController

--- tests/controllers/AppController.test.js
+++ tests/controllers/AppController.test.js
@@ -1,19 +1,26 @@
-import app from '../../server';
+/* eslint-disable jest/prefer-expect-assertions */
+/* eslint-disable no-unused-expressions */
+/* eslint-disable jest/valid-expect */
 import request from 'supertest';
 import { expect } from 'chai';
+import app from '../../server';
 
 describe('App Endpoints', () => {
-  it('should return status of redis and DB on GET /status', async () => {
-    const res = await request(app).get('/status');
-    expect(res.status).to.equal(200);
-    expect(res.body).to.have.property('redis', true);
-    expect(res.body).to.have.property('db', true);
+  describe('GET /status', () => {
+    it('should return status of redis and DB', async () => {
+      const res = await request(app).get('/status');
+      expect(res.status).to.equal(200);
+      expect(res.body).to.have.property('redis', true);
+      expect(res.body).to.have.property('db', true);
+    });
   });
 
-  it('should return number of users and files on GET /stats', async () => {
-    const res = await request(app).get('/stats');
-    expect(res.status).to.equal(200);
-    expect(res.body).to.have.property('users').that.is.a('number');
-    expect(res.body).to.have.property('files').that.is.a('number');
+  describe('GET /stats', () => {
+    it('should return number of users and files on GET /stats', async () => {
+      const res = await request(app).get('/stats');
+      expect(res.status).to.equal(200);
+      expect(res.body).to.have.property('users').that.is.a('number');
+      expect(res.body).to.have.property('files').that.is.a('number');
+    });
   });
 });
