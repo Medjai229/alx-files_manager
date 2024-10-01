67dcfd8d59656b9243e68e1b2ca02534ff61657a
Malik <malikhussein229@gmail.com>
2024-10-01 23:33:52 +0300

    Task#10 Task: GET /users/me

--- tests/controllers/UsersController.test.js
+++ tests/controllers/UsersController.test.js
@@ -5,36 +5,35 @@
 /* eslint-disable no-undef */
 import request from 'supertest';
 import { expect } from 'chai';
-import { ObjectId } from 'mongodb';
 import dbClient from '../../utils/db';
 import redisClient from '../../utils/redis';
 import app from '../../server';
 
 describe('User Endpoints', () => {
+  let userToken;
   let userId;
+  const userData = {
+    email: 'test@example.com',
+    password: '123456',
+  };
 
   describe('POST /users', () => {
-    const userData = {
-      email: 'test@example.com',
-      password: '123456',
-    };
     it('should create a new user', async () => {
       const res = await request(app).post('/users').send(userData);
       expect(res.status).to.equal(201);
       expect(res.body).to.have.property('id').that.is.a('string');
       expect(res.body).to.have.property('email', userData.email);
-
       userId = res.body.id;
     });
 
     it('should return 400 for missing email', async () => {
-      const res = await request(app).post('/users').send({ password: '123456' });
+      const res = await request(app).post('/users').send({ password: userData.password });
       expect(res.status).to.equal(400);
       expect(res.body).to.have.property('error', 'Missing email');
     });
 
     it('should return 400 for missing password', async () => {
-      const res = await request(app).post('/users').send({ email: 'test@example.com' });
+      const res = await request(app).post('/users').send({ email: userData.email });
       expect(res.status).to.equal(400);
       expect(res.body).to.have.property('error', 'Missing password');
     });
@@ -46,7 +45,34 @@ describe('User Endpoints', () => {
     });
   });
 
+  describe('GET /users/me', () => {
+    before(async () => {
+      const res = await request(app).get('/connect').auth(userData.email, userData.password);
+      userToken = res.body.token;
+    });
+
+    it('should return authenticated user information', async () => {
+      const res = await request(app).get('/users/me').set('x-token', userToken);
+      expect(res.status).to.equal(200);
+      expect(res.body).to.have.property('id', userId);
+      expect(res.body).to.have.property('email', userData.email);
+    });
+
+    it('should return 401 for unauthorized (no token)', async () => {
+      const res = await request(app).get('/users/me').set('x-token', '');
+      expect(res.status).to.equal(401);
+      expect(res.body).to.have.property('error', 'Unauthorized');
+    });
+
+    it('should return 401 for unauthorized (no user id found)', async () => {
+      const res = await request(app).get('/users/me').set('x-token', 'notAToken');
+      expect(res.status).to.equal(401);
+      expect(res.body).to.have.property('error', 'Unauthorized');
+    });
+  });
+
   after(async () => {
-    await dbClient.client.db().collection('users').deleteOne({ _id: ObjectId(userId) });
+    await dbClient.client.db().collection('users').deleteOne({ email: userData.email });
+    await redisClient.del(`auth_${userToken}`);
   });
 });
